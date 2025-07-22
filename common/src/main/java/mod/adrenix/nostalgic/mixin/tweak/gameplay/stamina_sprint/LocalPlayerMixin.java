920543a13f784bd73faf0ccc836f682cc475d743
Adrenix <15348635+adrenix@users.noreply.github.com>
2025-07-22 13:15:41 -0400

    fix(tweak): stamina exhaustion prevents player in creative from sprinting

--- common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/stamina_sprint/LocalPlayerMixin.java
+++ common/src/main/java/mod/adrenix/nostalgic/mixin/tweak/gameplay/stamina_sprint/LocalPlayerMixin.java
@@ -1,14 +1,26 @@
 package mod.adrenix.nostalgic.mixin.tweak.gameplay.stamina_sprint;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.authlib.GameProfile;
 import mod.adrenix.nostalgic.helper.gameplay.stamina.StaminaHelper;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LocalPlayer.class)
-public abstract class LocalPlayerMixin
+public abstract class LocalPlayerMixin extends AbstractClientPlayer
 {
+    /* Fake Constructor */
+
+    private LocalPlayerMixin(ClientLevel clientLevel, GameProfile gameProfile)
+    {
+        super(clientLevel, gameProfile);
+    }
+
+    /* Injections */
+
     /**
      * Ensures the local player cannot start sprinting if they are "exhausted" by the stamina sprint system. The server
      * will attempt to enforce this where possible.
@@ -19,7 +31,10 @@ public abstract class LocalPlayerMixin
     )
     private boolean nt_stamina_sprint$modifyCanStartSprinting(boolean canSprint)
     {
-        if (StaminaHelper.get((LocalPlayer) (Object) this).isExhausted())
+        if (this.isCreative() || this.isSpectator())
+            return canSprint;
+
+        if (StaminaHelper.get(this).isExhausted())
             return false;
 
         return canSprint;
